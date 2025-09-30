59fda1d29181d79b78bc0ca7293e72544c6ff5b7
Sinkó Dávid <sddavid118@gmail.com>
2025-09-30 08:10:23 +0200

    Model létrehozása

---
+++ Models/BarlangModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFeladat.Models
+{
+    public class BarlangModel
+    {
+
+        [Key] public int Id { get; set; }
+        public string Nev { get; set; }
+        public int Hossz { get; set; }
+        public int Kiterjedes { get; set; }
+        public int Melyseg { get; set; }
+        public int Magassag { get; set; }
+        public string Telepules { get; set; }
+    }
+}
